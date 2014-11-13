af5e9811804c76e17cbfaf4d22f17fe055da50c4
Stephen McMahon <stephen@silverstripe.com.au>
2014-11-13 17:02:44 +1100

    Add edismax solr query builder

--- code/service/SolrSearchService.php
+++ code/service/SolrSearchService.php
@@ -93,6 +93,7 @@ class SolrSearchService {
 
 		$this->queryBuilders['default'] = 'SolrQueryBuilder';
 		$this->queryBuilders['dismax'] = 'DismaxSolrSearchBuilder';
+		$this->queryBuilders['edismax'] = 'EdismaxSolrSearchBuilder';
 	}
 	
 	public function setCache($cache) {
---
+++ code/service/querybuilders/EdismaxSolrQueryBuilder.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * Query parser for Dismax querying
+ * 
+ * @author marcus@silverstripe.com.au
+ * @license BSD License http://silverstripe.org/bsd-license/
+ */
+class EdismaxSolrSearchBuilder extends SolrQueryBuilder {
+	public $title = 'Solr Edismax';
+	
+	public function toString() {
+		return $this->userQuery;
+	}
+
+	public function getParams() {
+		parent::getParams();
+		
+		$fields = '';
+		$sep = '';
+		foreach ($this->fields as $field) {
+			$fields .= $sep . $field;
+			if (isset($this->boost[$field])) {
+				$fields .= '^'.$this->boost[$field];
+			}
+			$sep = ' ';
+		}
+		$this->params['qf'] = $fields;
+		$this->params['defType'] = 'edismax';
+		
+		$filterVals = isset($this->params['fq']) ? $this->params['fq'] : array();
+		
+		foreach ($this->and as $field => $valArray) {
+			foreach ($valArray as $value) {
+				$filterVals[] = $field. ':' .$value;
+			}
+		}
+
+		$this->params['fq'] = $filterVals;
+		
+		$bq = array();
+		foreach ($this->boostFieldValues as $field => $boost) {
+			$bq[] = "$field^$boost";
+		}
+		if (count($bq)) {
+			$this->params['bq'] = $bq;
+		}
+		
+		if ($this->sort) {
+			$this->params['sort'] = $this->sort;
+		}
+		
+		$this->facetParams();
+		
+		return $this->params;
+	}
+}
